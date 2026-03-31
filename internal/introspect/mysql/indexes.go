5aa93ede4aedd92d58ddc0b9f0129bec8290971f
Pieczasz <bartekp854@gmail.com>
2026-03-31 11:19:14 +0200

    feat: parse indexes introspect more features

--- internal/introspect/mysql/indexes.go
+++ internal/introspect/mysql/indexes.go
@@ -75,24 +75,71 @@ func parseIndexPrefix(tokens []string, i int, idx *schema.Index) int {
 func parseIndexOptions(tokens []string, i int, idx *schema.Index) {
 	for i < len(tokens) {
 		token := strings.ToUpper(tokens[i])
-		switch token {
-		case "USING":
-			i++
-			if i < len(tokens) {
-				idx.Type = schema.IndexType(strings.ToUpper(tokens[i]))
-			}
-		case "COMMENT":
-			i++
-			if i < len(tokens) {
-				idx.Comment = introspect.StripQuotes(tokens[i])
-			}
-		case "VISIBLE":
+		switch {
+		case token == "USING":
+			i = parseUsingOption(tokens, i, idx)
+		case token == "COMMENT":
+			i = parseCommentOption(tokens, i, idx)
+		case token == "VISIBLE":
 			idx.Visibility = schema.IndexVisible
-		case "INVISIBLE":
+		case token == "INVISIBLE":
 			idx.Visibility = schema.IndexInvisible
+		case token == "KEY_BLOCK_SIZE" || strings.HasPrefix(token, "KEY_BLOCK_SIZE="):
+			i = parseKeyBlockSizeOption(tokens, i, token, idx)
+		case token == "WITH":
+			i = parseWithParserOption(tokens, i, idx)
 		}
 		i++
 	}
+}
+
+func parseUsingOption(tokens []string, i int, idx *schema.Index) int {
+	if i+1 < len(tokens) {
+		idx.Type = schema.IndexType(strings.ToUpper(tokens[i+1]))
+		return i + 1
+	}
+	return i
+}
+
+func parseCommentOption(tokens []string, i int, idx *schema.Index) int {
+	if i+1 < len(tokens) {
+		idx.Comment = introspect.StripQuotes(tokens[i+1])
+		return i + 1
+	}
+	return i
+}
+
+func parseKeyBlockSizeOption(tokens []string, i int, token string, idx *schema.Index) int {
+	if token == "KEY_BLOCK_SIZE" {
+		if i+1 < len(tokens) {
+			idxToParse := i + 1
+			if tokens[idxToParse] == "=" && idxToParse+1 < len(tokens) {
+				idxToParse++
+			} else if after, ok := strings.CutPrefix(tokens[idxToParse], "="); ok {
+				tokens[idxToParse] = after
+			}
+			if val, err := strconv.Atoi(tokens[idxToParse]); err == nil {
+				idx.KeyBlockSize = val
+			}
+			return idxToParse
+		}
+	} else {
+		parts := strings.SplitN(tokens[i], "=", 2)
+		if len(parts) == 2 {
+			if val, err := strconv.Atoi(parts[1]); err == nil {
+				idx.KeyBlockSize = val
+			}
+		}
+	}
+	return i
+}
+
+func parseWithParserOption(tokens []string, i int, idx *schema.Index) int {
+	if i+2 < len(tokens) && strings.ToUpper(tokens[i+1]) == "PARSER" {
+		idx.Parser = introspect.StripQuotes(tokens[i+2])
+		return i + 2
+	}
+	return i
 }
 
 func parseIndexColumns(idx *schema.Index, colsStr string) {
@@ -104,36 +151,44 @@ func parseIndexColumns(idx *schema.Index, colsStr string) {
 		colToken = strings.TrimSpace(colToken)
 		parts := introspect.Tokenize(colToken)
 		if len(parts) > 0 {
-			colIdx := schema.ColumnIndex{}
-			namePart := parts[0]
-
-			if parenIdx := strings.Index(namePart, "("); parenIdx != -1 {
-				colIdx.Name = introspect.StripQuotes(namePart[:parenIdx])
-				lenStr := strings.Trim(namePart[parenIdx:], "()")
-				if l, err := strconv.Atoi(lenStr); err == nil {
-					colIdx.Length = l
-				}
-			} else {
-				colIdx.Name = introspect.StripQuotes(namePart)
-			}
-
-			for j := 1; j < len(parts); j++ {
-				part := strings.ToUpper(parts[j])
-				switch {
-				case strings.HasPrefix(part, "("):
-					lenStr := strings.Trim(part, "()")
-					if l, err := strconv.Atoi(lenStr); err == nil {
-						colIdx.Length = l
-					}
-				case part == "ASC":
-					colIdx.Order = schema.SortAsc
-				case part == "DESC":
-					colIdx.Order = schema.SortDesc
-				}
-			}
-
+			colIdx := parseColumnIndex(parts)
 			idx.Columns = append(idx.Columns, colIdx)
 		}
 	}
 }
 
+func parseColumnIndex(parts []string) schema.ColumnIndex {
+	colIdx := schema.ColumnIndex{}
+	namePart := parts[0]
+
+	if strings.HasPrefix(namePart, "(") && strings.HasSuffix(namePart, ")") {
+		colIdx.Expression = strings.TrimSuffix(strings.TrimPrefix(namePart, "("), ")")
+	} else {
+		if parenIdx := strings.Index(namePart, "("); parenIdx != -1 {
+			colIdx.Name = introspect.StripQuotes(namePart[:parenIdx])
+			lenStr := strings.Trim(namePart[parenIdx:], "()")
+			if l, err := strconv.Atoi(lenStr); err == nil {
+				colIdx.Length = l
+			}
+		} else {
+			colIdx.Name = introspect.StripQuotes(namePart)
+		}
+	}
+
+	for j := 1; j < len(parts); j++ {
+		part := strings.ToUpper(parts[j])
+		switch {
+		case strings.HasPrefix(part, "("):
+			lenStr := strings.Trim(part, "()")
+			if l, err := strconv.Atoi(lenStr); err == nil {
+				colIdx.Length = l
+			}
+		case part == "ASC":
+			colIdx.Order = schema.SortAsc
+		case part == "DESC":
+			colIdx.Order = schema.SortDesc
+		}
+	}
+
+	return colIdx
+}
